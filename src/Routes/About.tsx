6bda9403e6eddec4a5cee7d01859532e4ee098e3
Gtv1521 <gustavober98@gmail.com>
2024-07-25 20:48:17 -0500

    feat: se agrega el modulo de about y se configura responsive

--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -13,16 +13,50 @@ function About() {
           <p>
             Tambien se trabajar con tecnologías como React, Node.js, MongoDB y otras.
 
+            Se manejar tecnoligias como Git, TFS, Jenkins, Docker, Kubernetes y Docker Swarm.
             {/* También he trabajado en proyectos de desarrollo de software para empresas en diferentes industrias, como tecnologías de seguimiento y control de calidad, sistemas de gestión de proyectos, y sistemas de información para empresas de retail y logística. */}
           </p>
           <p>
             Me gustaría trabajar en proyectos que me ayuden a desarrollar habilidades y a aplicar mis conocimientos en diferentes áreas de la empresa.
           </p>
           <p>
-            Mi correo electrónico es: <a href="mailto:gustavober98@gmail.com">gustavober98@gmail.com</a>
+            Mi correo electrónico es: <a href="mailto:gustavober98@gmail.com">gustavober98@gmail.com .</a>
           </p>
         </div>
       </article>
+
+      <div className="tecnoligias">
+        <h1 className='title'>Tecnologías</h1>
+
+        Tengo conocimientos en tecnoligias como:
+        <ul>
+          <li>
+            .NET Core
+          </li>
+          <li>
+            C#
+          </li>
+          <li>
+            JavaScript
+          </li>
+          <li>
+            React
+          </li>
+          <li>
+            Node.js
+          </li>
+          <li>
+            Html
+          </li>
+          <li>
+            CSS
+          </li>
+          <li>
+            Sass
+          </li>
+        </ul>
+      </div>
+
     </div>
   )
 }
